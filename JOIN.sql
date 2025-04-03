ba38a0e05c36df5a0cc27f7179d434af2c60c00d
Durdona <durdona.stu@gmail.com>
2025-04-03 14:55:30 -0500

    Update JOIN.sql

--- JOIN.sql
+++ JOIN.sql
@@ -2,7 +2,7 @@
 --Inner Join returns only overlapping data inside 2 tables, which are without nulls.
 
 Select 
-employees.name as employee_name,   --employee.name to retrieve names from employee table 
+employees.name as employee_name,   --employee.name to retrieve names from employee table (alias)
 employees.role as employee_role,
 departments.name as department_name
 From `astral-trees-454812-s3.employee_data.employees` as employees
@@ -29,3 +29,21 @@ departments.name as department_name
 From `astral-trees-454812-s3.employee_data.employees` as employees
 Left Join `astral-trees-454812-s3.employee_data.departments` as departments
 On employees.department_id= departments.department_id
+
+
+---------------------------------------------------------------------------------------
+--In 2015, how many people were of the official age for secondary education broken down by region of the world?
+
+SELECT
+summary.region,
+SUM(edu.value) secondary_edu_population      --Use the SUM(value) to calculate the total population for a given size
+FROM
+    `bigquery-public-data.world_bank_intl_education.international_education` AS edu
+Inner JOIN
+    `bigquery-public-data.world_bank_intl_education.country_summary` AS summary
+ON edu.country_code = summary.country_code        --country_code is our key
+    WHERE summary.region IS NOT NULL              --Exclude rows with a missing region.
+    AND edu.indicator_name = 'Population of the official age for secondary education, both sexes (number)'
+    AND edu.year = 2015
+GROUP BY summary.region
+ORDER BY secondary_edu_population DESC      --Sort by highest population region first.
